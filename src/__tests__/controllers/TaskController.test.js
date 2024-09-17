f5e61156a51b89d027d4c85eafcf48054a3df217
ManForProm <manforprom@gmail.com>
2024-09-17 13:53:48 +0300

    added inversify to tests

--- src/__tests__/controllers/TaskController.test.js
+++ src/__tests__/controllers/TaskController.test.js
@@ -1,20 +1,27 @@
+import { Container } from 'inversify';
 import 'reflect-metadata';
 import TaskController from '../../controllers/TaskController.js';
 import TaskService from '../../services/TaskService.js';
+import { ValidationProvider } from '../../utils/ValidationProvider.js';
 
+let TaskServiceMock;
+let container = null;
 jest.mock('../../services/TaskService.js');
 jest.mock('../../utils/ValidationProvider.js');
 
-describe('TaskController', () => {
+describe('TaskController with Inversify (JavaScript)', () => {
   let taskController;
   let mockReq;
   let mockRes;
 
   beforeEach(() => {
-    const mockTaskService = new TaskService();
-    const mockValidationProvider = jest.fn();
-    taskController = new TaskController(mockTaskService, mockValidationProvider);
+    container = new Container();
+    container.bind('TaskController').to(TaskController);
+    container.bind('TaskService').to(TaskService);
+    container.bind('ValidationProvider').to(ValidationProvider);
+    taskController = container.get('TaskController');
 
+    // Мокаем запрос и ответ
     mockReq = {
       params: {},
       body: {}
@@ -27,6 +34,7 @@ describe('TaskController', () => {
 
   test('should get all tasks', async () => {
     const tasks = [{ id: 1, title: 'Test Task' }];
+    
     taskController._taskService.getAllTasks.mockResolvedValue(tasks);
 
     await taskController.getTasks(mockReq, mockRes);
@@ -35,25 +43,17 @@ describe('TaskController', () => {
     expect(mockRes.json).toHaveBeenCalledWith(tasks);
   });
 
-  test('should create task', async () => {
+  test('should create a task', async () => {
     const newTask = { id: 1, title: 'New Task' };
-    mockReq.body = {
-      title: 'New Task',
-      discription: 'Task description',
-      complete: false,
-      userEmail: 'test@example.com'
-    };
+    mockReq.body = { title: 'New Task', discription: 'Task description', complete: false, userEmail: 'test@example.com' };
     taskController._taskService.createTask.mockResolvedValue(newTask);
-
+    
     await taskController.createTask(mockReq, mockRes);
-
+    
     expect(taskController._taskService.createTask).toHaveBeenCalledWith(
-      'New Task',
-      'Task description',
-      false,
-      'test@example.com'
+      'New Task', 'Task description', false, 'test@example.com'
     );
-    expect(mockRes.status).toHaveBeenCalledWith(201);
+    expect(mockRes.status).toHaveBeenCalledWith(201);  // Проверяем статус 201
     expect(mockRes.json).toHaveBeenCalledWith(newTask);
   });
 });
